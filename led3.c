e3ab7d590a20f3c34cc3f9c8b5821627a49e192e
tsotnep <tsotnep@gmail.com>
2016-02-09 21:43:48 +0200

    led3.c finalized

--- led3.c
+++ led3.c
@@ -9,20 +9,27 @@
 #include <linux/kobject.h>    // using kobjects for the sysfs bindings
 #include <linux/kthread.h>    // for using kthreads in the led flashing functionality
 #include <linux/delay.h>  // msleep() function
+#include <linux/interrupt.h>    // for IRQ code
  
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("John Doe");
-MODULE_DESCRIPTION("LED sysfs interface driver");
+MODULE_AUTHOR("Tsotnep");
+MODULE_DESCRIPTION("LED sysfs interface driver SWitch controlled");
 MODULE_VERSION("0.1");
  
 static unsigned int gpioLED = 61;
+static unsigned int gpioSW = 69;
+static unsigned int irqNumber;
 static bool ledState = 0;
+static bool swState = 0;
 static char ledName[7] = "myled";
 static unsigned int period = 1000; // blink period in ms
  
 enum modes { OFF, ON, BLINK };      // enum for LED modes
 static enum modes mode = ON;        // default mode off
  
+// function prototype for custom irq handler
+static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
+
 // Callback function to display the LED mode
 static ssize_t mode_show(struct  kobject *kobj, struct kobj_attribute *attr, char *buf) {
     switch(mode) {
@@ -79,10 +86,14 @@ static struct task_struct *task;    // pointer to the thread task
 // kthread loop to blink the LED
 static int blink(void *arg) {
     while(!kthread_should_stop()) {     //returns true when kthread_stop() is called
-        set_current_state(TASK_RUNNING);
-        if(mode==BLINK) ledState = !ledState;  // if the mode is blink, then invert LED state
-        else if(mode==ON) ledState = true;
-        else ledState = false;
+        if (swState == false ){
+            ledState = false;
+        }
+        else {
+            if(mode==BLINK) ledState = !ledState;  // if the mode is blink, then invert LED state
+            else if(mode==ON) ledState = true;
+            else ledState = false;
+        }
         gpio_set_value(gpioLED, ledState); // send the LED state value to the GPIO
         set_current_state(TASK_INTERRUPTIBLE);
         msleep(period/2);       // sleep half of the period (cycle of on+off is one period)
@@ -125,18 +136,44 @@ static int __init gpio_init(void) {
     gpio_direction_output(gpioLED, ledState); // set gpio to be in output mode, second parameter will set the value
     gpio_export(gpioLED, false);        // export will make the gpio pin to appear in /sys/class/gpio
                         // second parameter will prevent direction from being changed
+    gpio_request(gpioSW, "sysfs");          // set up gpio switch
+    gpio_direction_input(gpioSW);            // set switch as input
+    gpio_set_debounce(gpioSW, 200);         // debounce with delay of 200ms
+    gpio_export(gpioSW, false);             // make it appear in /sys/class/gpio
  
+    printk(KERN_INFO "GPIO_LED: switch currently in state %d\n", gpio_get_value(gpioSW));
+ 
+    // GPIO numbers and IRQ numbers are not the same and this function will map them
+    irqNumber = gpio_to_irq(gpioSW); //what if i create another irqNumber-s for other GPIO-s, simly - array.
+    printk(KERN_INFO "GPIO_LED: switch %d is mapped to IRQ %d\n", gpioSW, irqNumber); 
+    // request an interrupt line
+    result = request_irq(irqNumber,                             // interrupt number requested
+                    (irq_handler_t) gpio_irq_handler,           // pointer to the handler function
+                    IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, // trigger both on rising and falling signal edge
+                    "ledswitch_gpio_handler",                   // used in /proc/interrupts to identify the owner
+                    NULL);                                      // *dev_id for shared interrupt lines
+    printk(KERN_INFO "GPIO_LED: the interrupt request result is: %d\n", result);
+
     return result;
 }
  
 static void __exit gpio_exit(void) {
+    printk(KERN_INFO "GPIO_LED: The switch state is currently %d\n", gpio_get_value(gpioSW));
     kthread_stop(task);     // stop LED blinking thread
     kobject_put(led_kobj);  // clean the kobject sysfs entry
     gpio_set_value(gpioLED, 0);
+    free_irq(irqNumber, NULL);
     gpio_unexport(gpioLED);
+    gpio_unexport(gpioSW);
     gpio_free(gpioLED);
+    gpio_free(gpioSW);
+
     printk(KERN_INFO "GPIO_LED: Module unloaded\n");
 }
  
+static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs) {
+    swState = gpio_get_value(gpioSW); // invert led state
+    return (irq_handler_t) IRQ_HANDLED; // announce that the IRQ has been handled correctly
+}
 module_init(gpio_init);
 module_exit(gpio_exit);
